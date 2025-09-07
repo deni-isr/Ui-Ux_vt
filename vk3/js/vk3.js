2df56664c717832c45f191b7089f7dab5bc75de0
Deni <mrthelink09@gmail.com>
2025-09-07 20:40:48 +0300

    tehtävä 3 on tehty

--- vk3/js/vk3.js
+++ vk3/js/vk3.js
@@ -1,11 +1,19 @@
-const dialog = document.querySelector('dialog');
-const showButton = document.querySelector('#showdialog');
-const closeButton = document.querySelector('dialog button');
+const dialog = document.querySelector('#signupDialog');
+const showButton = document.querySelector('#showdialog-dup');
+const closeButton = document.querySelector('#closeDialog');
 
-showButton.addEventListener('click', () => {
+function openDialog() {
   dialog.showModal();
-});
+  dialog.querySelector('input')?.focus();
+}
 
-closeButton.addEventListener('click', () => {
-  dialog.close();
+showButton.addEventListener('click', openDialog);
+closeButton.addEventListener('click', () => dialog.close());
+
+dialog.addEventListener('click', (e) => {
+  const rect = dialog.getBoundingClientRect();
+  const inDialog =
+    e.clientX >= rect.left && e.clientX <= rect.right &&
+    e.clientY >= rect.top  && e.clientY <= rect.bottom;
+  if (!inDialog) dialog.close();
 });
